33cd731488e4fb3c0149446b1f5ac1681d12b145
mugman <53713683+nature174@users.noreply.github.com>
2021-08-11 17:43:29 -0700

    Add GPLv3 License

--- inject.py
+++ inject.py
@@ -1,4 +1,24 @@
+"""
+    The Fosscord Injector injects json into a Discord client's settings file,
+    so it may connect to a Fosscord instance.
+    Copyright (C) 2021  mugman174
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
 import sys, os, json
+
 
 try:
 	DRY_RUN = bool(os.environ["DRY_RUN"])
