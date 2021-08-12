e5daef9341d14bdb005f0c15575cc219f6be86f3
mugman <53713683+nature174@users.noreply.github.com>
2021-08-11 17:31:03 -0700

    First Commit - Add the injector

---
+++ inject.py
@@ -0,0 +1,61 @@
+import sys, os, json
+
+try:
+	DRY_RUN = bool(os.environ["DRY_RUN"])
+except KeyError:
+	DRY_RUN = False
+
+print(f"""
+ _____                               _
+|  ___|__  ___ ___  ___ ___  _ __ __| |
+| |_ / _ \/ __/ __|/ __/ _ \| '__/ _` |
+|  _| (_) \__ \__ \ (_| (_) | | | (_| |
+|_|  \___/|___/___/\___\___/|_|  \__,_|
+
+Fosscord Injector v0.0.1 by mugman
+
+{"[Dry Run]" if DRY_RUN else ''}
+""")
+
+
+end_url = input("Enter your Fosscord instance IP (e.x. '127.0.0.1:3001', 'myvps.com:3001', or 'app.fosscord.com'): ")
+https = input("Does the instance support HTTPS? (Does it have a valid HTTP cert?) [yes,no]: ")
+https = 'https' if https else 'http'
+print("1 - Discord Stable\n2 - Discord PTB\n3 - Discord Canary\n4. Discord Development")
+ver_in = int(input("Choose your version [1,2,3,4]: "))
+
+
+if ver_in == 1:
+	version = "discord"
+elif ver_in == 2:
+	version = "discordptb"
+elif ver_in == 3:
+	version = "discordcanary"
+elif ver_in == 4:
+	version = "discorddevelopment"
+else:
+	raise Exception("Invalid Choice/Number. Choose from the options [1,2,3,4].")
+
+os_ver = sys.platform
+if "linux" in os_ver:
+	confpath = f"""{os.environ["HOME"]}/.config/{version}/"""
+elif "win32" in os_ver:
+	confpath = f"""{os.environ["APPDATA"]}\\{version}"""
+elif "darwin" in os_ver:
+	confpath = f"""{os.environ["HOME"]}/Library/Application Support/{version}/"""
+else:
+	raise Exception(f"{os_ver} is not a supported platform.")
+try:
+	os.chdir(confpath)
+except FileNotFoundError:
+	raise FileNotFoundError("The Discord version you have inputted is invalid, or your config directory is different from the default. Please report this problem to mugman#5453 on Discord.")
+
+inject_json = {"API_ENDPOINT": f"{https}://{end_url}/api/v8","WEBAPP_ENDPOINT": f"{https}://{end_url}","UPDATE_ENDPOINT": "https://updates.goosemod.com/goosemod","NEW_UPDATE_ENDPOINT": "https://updates.goosemod.com/goosemod/"}
+
+if not DRY_RUN:
+	print("Writing Files...")
+	with open("settings.json", "w") as settings_file:
+		json.dump(inject_json, settings_file)
+		print(f"Injected! Please restart your Discord client ({version}), and enjoy!")
+else:
+	print("Dry Run Complete.")
